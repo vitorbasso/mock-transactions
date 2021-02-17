0e4784947328dcc48d56bd11cf07c3d95d223b74
Vitor Basso <vitorbasso93@gmail.com>
2021-02-16 23:25:20 -0300

    returning more than one transaction
    - Generating the correct number of transactions to return
    - Some refactoring

--- src/main/kotlin/br/com/basso/mocktransactions/repository/TransactionRepository.kt
+++ src/main/kotlin/br/com/basso/mocktransactions/repository/TransactionRepository.kt
@@ -1,13 +1,9 @@
 package br.com.basso.mocktransactions.repository
 
 import br.com.basso.mocktransactions.entity.Transaction
+import br.com.basso.mocktransactions.util.RandomUtil
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
-import java.sql.Timestamp
-import java.time.LocalDate
-import java.time.LocalTime
-import java.time.YearMonth
-import kotlin.random.Random
 
 @Component
 class TransactionRepository(
@@ -22,76 +18,17 @@ class TransactionRepository(
 ) {
 
     fun getTransactions(userId: Int, transactionYear: Int, transactionMonth: Int): List<Transaction> {
-        val random = getRandomObject(userId, transactionYear, transactionMonth)
-        return listOf(
-            Transaction(
-                descricao = getRandomDescription(random),
-                data = getRandomDate(
-                    transactionYear = transactionYear,
-                    transactionMonth = transactionMonth,
-                    random = random
-                ),
-                valor = getRandomValue(random)
-            )
-        )
-    }
-
-    private fun getRandomDescription(random: Random): String {
-        val wordList = mutableListOf<String>()
-        var phraseSpaceLeft = random.nextInt(minDescriptionSize, maxDescriptionSize + 1)
-        while (phraseSpaceLeft > MIN_WORD_SIZE) {
-            random.nextInt(MIN_WORD_SIZE, MAX_WORD_SIZE).also { wordSize ->
-                wordList.add(getRandomWord(wordSize, random))
-                phraseSpaceLeft -= wordSize
-            }
+        val random = RandomUtil.getInstance(userId, transactionYear, transactionMonth)
+        val numberOfTransactions = userId.toString().substring(0,1).toInt() * transactionMonth
+        return List(numberOfTransactions) {
+            getRandomTransaction(random)
         }
-        return wordList.joinToString(separator = " ", limit = maxDescriptionSize)
     }
 
-    private fun getRandomWord(wordSize: Int, random: Random): String {
-        var isVowelTurn = random.nextBoolean()
-        var word = ""
-        for (i in 0..wordSize) {
-            word += if (isVowelTurn) VOWEL_LIST[random.nextInt(0, VOWEL_LIST.size)]
-            else CONSONANT_LIST[random.nextInt(0, CONSONANT_LIST.size)]
-            isVowelTurn = !isVowelTurn
-        }
-        return word
-    }
-
-    private fun getRandomValue(random: Random) = random.nextInt(minValue, maxValue)
-
-    private fun getRandomDate(
-        transactionYear: Int,
-        transactionMonth: Int,
-        random: Random
-    ) = Timestamp.valueOf(
-        LocalDate.of(
-            transactionYear,
-            transactionMonth,
-            random.nextInt(1, YearMonth.of(transactionYear, transactionMonth).lengthOfMonth() + 1)
-        ).atTime(
-            LocalTime.of(
-                random.nextInt(RANGE_TIME.first, RANGE_HOUR.second),
-                random.nextInt(RANGE_TIME.first, RANGE_TIME.second),
-                random.nextInt(RANGE_TIME.first, RANGE_TIME.second)
-            )
-        )
-    ).time
-
-    //generates a Random object with a seed number that is statistically improbable to be repeated given different
-    //values for a, b and c
-    private fun getRandomObject(a: Int, b: Int, c: Int) = Random((a * BIGGER_PRIME_NUMBER + b) * BIG_PRIME_NUMBER + c)
-
-    companion object {
-        private const val BIG_PRIME_NUMBER = 31L
-        private const val BIGGER_PRIME_NUMBER = 79L
-        private const val MIN_WORD_SIZE = 2
-        private const val MAX_WORD_SIZE = 14
-        private val RANGE_TIME = Pair(0, 60)
-        private val RANGE_HOUR = Pair(0, 24)
-        private val VOWEL_LIST = listOf('a', 'e', 'i', 'o', 'u')
-        private val CONSONANT_LIST = CharRange('a', 'z').filter { !VOWEL_LIST.contains(it) }
-    }
+    private fun getRandomTransaction(random: RandomUtil) = Transaction(
+        descricao = random.getRandomDescription(minDescriptionSize, maxDescriptionSize),
+        data = random.getRandomDate(),
+        valor = random.getRandomValue(minValue, maxValue)
+    )
 
 }
---
+++ src/main/kotlin/br/com/basso/mocktransactions/util/RandomUtil.kt
@@ -0,0 +1,61 @@
+package br.com.basso.mocktransactions.util
+
+import java.sql.Timestamp
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.YearMonth
+import kotlin.random.Random
+
+class RandomUtil private constructor(userId: Int, private val transactionYear: Int, private val transactionMonth: Int) {
+
+    //generates a Random object with a seed number that is statistically improbable to be repeated given different
+    //values for a, b and c
+    private val seed = (userId * BIGGER_PRIME_NUMBER + transactionYear) * BIG_PRIME_NUMBER + transactionMonth
+
+    private val random = Random(seed)
+
+    fun getRandomDate() = Timestamp.valueOf(
+        LocalDate.of(
+            transactionYear,
+            transactionMonth,
+            random.nextInt(1, YearMonth.of(transactionYear, transactionMonth).lengthOfMonth() + 1)
+        ).atTime(
+            LocalTime.of(
+                random.nextInt(RANGE_TIME.first, RANGE_HOUR.second),
+                random.nextInt(RANGE_TIME.first, RANGE_TIME.second),
+                random.nextInt(RANGE_TIME.first, RANGE_TIME.second)
+            )
+        )
+    ).time
+
+    fun getRandomValue(start: Int, end: Int) = random.nextInt(start, end)
+
+    fun getRandomDescription(minDescriptionSize: Int, maxDescriptionSize: Int) =
+        List(maxDescriptionSize / MIN_WORD_SIZE) {
+            getRandomWord(getRandomValue(MIN_WORD_SIZE, MAX_WORD_SIZE))
+        }.joinToString(separator = " ")
+            .substring(0, getRandomValue(minDescriptionSize + 1, maxDescriptionSize))
+            .trim()
+            .capitalize()
+
+    private fun getRandomWord(wordSize: Int): String {
+        var isVowelTurn = random.nextBoolean()
+        return List(wordSize) {
+            isVowelTurn = !isVowelTurn
+            if (isVowelTurn) VOWEL_LIST[random.nextInt(0, VOWEL_LIST.size)]
+            else CONSONANT_LIST[random.nextInt(0, CONSONANT_LIST.size)]
+        }.joinToString(separator = "")
+    }
+
+    companion object {
+        fun getInstance(a: Int, b: Int, c: Int) = RandomUtil(a, b, c)
+        private const val BIG_PRIME_NUMBER = 31L
+        private const val BIGGER_PRIME_NUMBER = 79L
+        private const val MIN_WORD_SIZE = 2
+        private const val MAX_WORD_SIZE = 12
+        private val RANGE_TIME = Pair(0, 60)
+        private val RANGE_HOUR = Pair(0, 24)
+        private val VOWEL_LIST = listOf('a', 'e', 'i', 'o', 'u')
+        private val CONSONANT_LIST = CharRange('a', 'z').filter { !VOWEL_LIST.contains(it) }
+    }
+}
